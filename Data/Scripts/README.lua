fcbca33f41ddae13030ce9d7b5079afa9fcd2f88
CommanderFoo <pixeldepth@gmail.com>
2022-10-05 16:22:03 +0100

    Update README.lua

--- Data/Scripts/README.lua
+++ Data/Scripts/README.lua
@@ -22,13 +22,13 @@ How to use
 Add the World Markers template to the hierarchy. The template comes with 2 examples created to show
 you how easy it is to creake a marker.
 
-The compoment also supports removing and adding markers as runtime. This is useful for removing a 
+The compoment also supports removing and adding markers as runtime. This is useful for clearing a 
 marker that the player has visited. Or adding a new marker for a quest.
 
-To add and remove markers, broadcast to either the `Marker.Add or `Marker.Remove` events, and pass
-the key that you wish to lookup and add or remove.
+To add and remove markers, broadcast to either the Marker.Add or Marker.Remove events, and pass
+the key that you wish to look up and add or remove.
 
-A data table called `World Markers` is where all of the markers need to go. The first column is the
+A data table called World Markers is where all of the markers need to go. The first column is the
 unqiue key for that markert. The icon is the icon to spawn in for that markert. The target is the 
 target object in the world the marker icon should point to.
 
@@ -36,7 +36,7 @@ There are a couple of custom properties on the root of the template.
 
 - EdgeOffset
 
-Distance the icon is from the edge of the screen.
+The distance the icon is from the edge of the screen.
 
 - WorldOffset
 
