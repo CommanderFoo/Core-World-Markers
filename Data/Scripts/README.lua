ee667267e75913ad5afe1076e1bc191703f1ee75
CommanderFoo <pixeldepth@gmail.com>
2022-10-05 16:27:45 +0100

    Update README.lua

--- Data/Scripts/README.lua
+++ Data/Scripts/README.lua
@@ -1,7 +1,7 @@
 --[[
- __          __        _     _   __  __            _                 
- \ \        / /       | |   | | |  \/  |          | |                
-  \ \  /\  / /__  _ __| | __| | | \  / | __ _ _ __| | _____ _ __ ___ 
+ __          __        _     _   __  __            _
+ \ \        / /       | |   | | |  \/  |          | |
+  \ \  /\  / /__  _ __| | __| | | \  / | __ _ _ __| | _____ _ __ ___
    \ \/  \/ / _ \| '__| |/ _` | | |\/| |/ _` | '__| |/ / _ \ '__/ __|
     \  /\  / (_) | |  | | (_| | | |  | | (_| | |  |   <  __/ |  \__ \
      \/  \/ \___/|_|  |_|\__,_| |_|  |_|\__,_|_|  |_|\_\___|_|  |___/
@@ -20,16 +20,16 @@ How to use
 ==========
 
 Add the World Markers template to the hierarchy. The template comes with 2 examples created to show
-you how easy it is to creake a marker.
+you how easy it is to create a marker.
 
-The compoment also supports removing and adding markers as runtime. This is useful for clearing a 
+The component also supports removing and adding markers as runtime. This is useful for clearing a
 marker that the player has visited. Or adding a new marker for a quest.
 
 To add and remove markers, broadcast to either the Marker.Add or Marker.Remove events, and pass
 the key that you wish to look up and add or remove.
 
 A data table called World Markers is where all of the markers need to go. The first column is the
-unqiue key for that markert. The icon is the icon to spawn in for that markert. The target is the 
+unique key for that marker. The icon is the icon to spawn in for that marker. The target is the
 target object in the world the marker icon should point to.
 
 There are a couple of custom properties on the root of the template.
